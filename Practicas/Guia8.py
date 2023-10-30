11fc19d27111e55a9c1c7d9b0913111b2dd7cddd
Tobiasgc.tech <clinux01@pc54.lab1106>
2023-10-30 10:05:02 -0300

    Guia8

--- Practicas/Guia8.py
+++ Practicas/Guia8.py
@@ -23,4 +23,28 @@ def cantidad_apariciones(nombre_archivo:str,palabra:str) -> int:
     contador = contenido.count(palabra)
     archivo.close()
     return contador
-print(cantidad_apariciones("Prueba.txt","Hola"))
+#print(cantidad_apariciones("Prueba.txt","Hola"))
+
+#Ejercicio2
+def clonar_sin_comentarios(nombre_archivo:str):
+    archivo = open(nombre_archivo,"r")
+    archivo_sin_comen = open("clonadoSinComentarios.py","w")
+    lineas = archivo.readlines()
+    for linea in lineas:
+        if not linea.strip()[0] == "#":
+            archivo_sin_comen.write(linea)
+    archivo.close()
+    archivo_sin_comen.close()
+#clonar_sin_comentarios("ejemploComentario.py")
+
+#Ejercicio3
+def reverso(nombre_archivo:str):
+    archivo = open(nombre_archivo,"r")
+    reverso = open ("reverso.txt","w")
+    lineas = archivo.readlines()
+    lineas_inversas = []
+    for linea in range (len(lineas)-1,-1,-1):
+        lineas_inversa 
+    archivo.close()
+    reverso.close()
+reverso("Prueba.txt") 
