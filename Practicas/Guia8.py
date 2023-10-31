b3131f52e8bf34da0709fea0a6c8ea1425b4fce8
Tobiasgc <tobiasgabrielcogliano@gmail.com>
2023-10-31 20:43:46 -0300

    Guia 8

--- Practicas/Guia8.py
+++ Practicas/Guia8.py
@@ -59,4 +59,154 @@ def generar_nros_al_azar(cantidad:int,desde:int,hasta:int) -> Pila:
         p.put(numero)
     print(list(p.queue))
     return p
-generar_nros_al_azar(10,0,10)
+#generar_nros_al_azar(10,0,10)
+
+#Ejercio9
+def cantidadElementos(p:Pila)-> int:
+    res: int=0
+    paux: Pila=Pila()
+    while not p.empty():
+        elem = p.get()
+        paux.put(elem)
+        res += 1
+    print(list(paux.queue))
+    while not paux.empty():
+        elem = paux.get()
+        p.put(elem)
+    print(list(p.queue))
+    return res
+"""""
+p = Pila()
+p.put(1)
+p.put(2)
+p.put(3)
+p.put(4)
+cantidad = cantidadElementos(p)
+print("Cantidad de elementos en la pila:", cantidad)
+print(list(p.queue))
+"""
+#Ejercio10
+def buscar_el_maximo(p:Pila) -> int:
+    res: int = 0
+    paux: Pila = Pila()
+    lista: list = []
+    while not p.empty():
+        elem = p.get()
+        paux.put(elem)
+        lista.append(elem)
+    while not paux.empty():
+        elem=paux.get()
+        p.put(elem)
+    for i in lista:
+        if i >= res:
+            res = i
+    return res
+"""      
+p = Pila()
+p.put(1)
+p.put(1000)
+p.put(5)
+p.put(4)
+cantidad = buscar_el_maximo(p)
+print(cantidad)
+"""
+#Ejercio11
+def esta_bien_balanceada(operacion:str) -> bool:
+    p: Pila = Pila()
+    for caracter in operacion:
+        if caracter == "(":
+            p.put(caracter)
+        elif caracter == ")":
+            if not p.empty():
+                p.get()
+            else:    
+                return False         
+    return p.empty()
+""""
+formula = "1 + 2 x 3 = (20 / 5))"
+resultado = esta_bien_balanceada(formula)
+print(resultado)
+"""
+#Ejercicio12
+def es_numero(cadena: str) -> bool:
+    try:
+        float(cadena)
+        return True
+    except ValueError:
+        return False
+
+def polacaInversa (expresion:str) -> float:
+    tokens = []
+    token_actual = ""
+    for caracter in expresion:
+        if caracter != " ":
+            token_actual += caracter
+        elif token_actual:
+            tokens.append(token_actual)
+            token_actual = ""
+    if token_actual:
+        tokens.append(token_actual)
+    print(tokens)
+    operando: Pila = Pila()
+    for i in tokens:
+        a: float
+        b: float
+        if es_numero(i):
+            operando.put(float(i))
+        elif i == "+":
+            a = int(operando.get())
+            b = int(operando.get())
+            operando.put(b+a)
+        elif i == "-":
+            a = int(operando.get())
+            b = int(operando.get())
+            operando.put(b-a)
+        elif i == "*":
+            a = int(operando.get())
+            b = int(operando.get())
+            operando.put(b*a)
+        elif i == "/":
+            a = int(operando.get())
+            b = int(operando.get())
+            operando.put(b/a)
+    print(list(operando.queue))
+    return operando
+#polacaInversa("3 4 + 5 * 2 -")
+
+#Ejercicio13
+from queue import Queue as Cola
+
+def generar_nros_al_azar1 (cantidad:int,desde:int,hasta:int) -> Cola:
+    c = Cola()
+    while cantidad > 0:
+        cantidad -= 1
+        numero: int = random.randint(desde,hasta)
+        c.put(numero)
+    print(list(c.queue))
+    return c
+#generar_nros_al_azar(10,0,10)
+
+#Ejercicio14
+def cantidad_elementos (c:Cola) -> int:
+    res:int = 0
+    caux: Cola = Cola()
+    while not c.empty():
+        elem = c.get()
+        caux.put(elem)
+        res += 1
+    print(list(caux.queue))
+    while not caux.empty():
+        elem = caux.get()
+        c.put(elem)
+    print(list(c.queue))
+    return res
+"""
+c = Cola()
+c.put(1)
+c.put(2)
+c.put(3)
+c.put(4)
+cantidad = cantidadElementos(c)
+print("Cantidad de elementos en la cola:", cantidad)
+print(list(c.queue))
+"""
