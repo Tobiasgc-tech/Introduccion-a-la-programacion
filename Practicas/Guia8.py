7aeb41bec52fceea47824472c2b159a58078098d
Tobiasgc <tobiasgabrielcogliano@gmail.com>
2023-11-01 16:41:14 -0300

    Guia 8

--- Practicas/Guia8.py
+++ Practicas/Guia8.py
@@ -210,3 +210,130 @@ cantidad = cantidadElementos(c)
 print("Cantidad de elementos en la cola:", cantidad)
 print(list(c.queue))
 """
+#Ejercio16
+def armarSecuenciaBingo () -> Cola:
+    numeros: list = list(range(1,100))
+    random.shuffle(numeros)
+    c: Cola = Cola()
+    for numero in numeros:
+        c.put(numero)
+    return c
+#armarSecuenciaBingo()
+
+def jugarCartonBingo (carton:list,bolillero:Cola) -> int:
+    res: int = 0
+    contador: int = 0
+    caux: Cola = Cola()
+    print(list(bolillero.queue))
+    while not bolillero.empty():
+        elem = bolillero.get()
+        caux.put(elem)
+        if elem in carton:
+            contador += 1
+            res += 1
+        else:
+            res += 1
+        if contador >= 12:
+            break
+    while not caux.empty():
+        elem = caux.get()
+        bolillero.put(elem)
+    print(list(bolillero.queue))
+    return res
+
+#print(jugarCartonBingo([1,2,3,4,5,6,7,8,9,77,11,12],armarSecuenciaBingo()))
+
+def n_pacientes_urgentes(c:Cola) -> int:
+    res: int = 0
+    caux: Cola = Cola()
+    while not c.empty():
+        elem = c.get()
+        caux.put(elem)
+        if 1 <= elem[0] <=3:
+            res += 1
+    while not caux.empty():
+        elem1 = caux.get()
+        c.put(elem1)
+    #print(list(c.queue))    
+    return res
+"""
+cola = Cola()
+cola.put((1,"Tobias","Tra"))
+cola.put((10,"Tobias","Tra"))
+cola.put((3,"Tobias","Tra"))
+cola.put((2,"Tobias","Tra"))
+cola.put((7,"Tobias","Tra"))
+cola.put((7,"Tobias","Tra"))
+cola.put((2,"Tobias","Tra"))
+cola.put((3,"Tobias","Tra"))
+cola.put((5,"Tobias","Tra"))
+print(list(cola.queue)) 
+print(n_pacientes_urgentes(cola))
+"""
+def clientes(cola:Cola) -> Cola:
+    clientesMas65: Cola = Cola()
+    clientesPrefe: Cola = Cola()
+    clientesComunes: Cola = Cola()
+    clientesOrdenados: Cola = Cola()
+    colaaux: Cola = Cola()
+    while not cola.empty():
+        cliente = cola.get()
+        if cliente[3] == True:
+            clientesMas65.put(cliente)
+        elif cliente[2] == True:
+            clientesPrefe.put(cliente)
+        else:
+            clientesComunes.put(cliente)
+        colaaux.put(cliente)
+    while not clientesMas65.empty():
+        cliente = clientesMas65.get()
+        clientesOrdenados.put(cliente)
+    while not clientesPrefe.empty():
+        cliente = clientesPrefe.get()
+        clientesOrdenados.put(cliente)
+    while not clientesComunes.empty():
+        cliente = clientesComunes.get()
+        clientesOrdenados.put(cliente)
+    while not colaaux.empty():
+        cliente = colaaux.get()
+        cola.put(cliente)
+    #print(list(cola.queue))
+    return clientesOrdenados
+"""
+cola = Cola()
+cola.put(("Tobias Cogliano",6,False,False))
+cola.put(("Tobias Cogliano",4,True,False))
+cola.put(("Tobias Cogliano",1,False,True))
+cola.put(("Tobias Cogliano",2,True,True))
+cola.put(("Tobias Cogliano",5,True,False))
+cola.put(("Tobias Cogliano",7,False,False))
+cola.put(("Tobias Cogliano",3,False,True))
+cola.put(("Tobias Cogliano",8,False,False))
+print(list(cola.queue))
+clientes(cola)
+"""
+#Ejercicio19
+def agruparPorLongitud(nombre_archivo:str) -> dict:
+    archivo = open(nombre_archivo,"r")
+    d: dict = {}
+    for linea in archivo.readlines():
+        palabras = linea.split()
+        for palabra in palabras:
+            clave = len(palabra)
+            if clave in d:
+                d[clave] += 1
+            else:
+                d[clave] = 1
+    archivo.close()
+    return d
+print(agruparPorLongitud("Prueba.txt"))
+
+#Ejercicio20
+from Guia7 import aprobado
+def promedio () -> dict:
+    d: dict = {}
+    alumnos: list = []
+    while True:
+        libreta = input("Ingresa LU (o '0' para terminar):")
+        if libreta != 0:
+
